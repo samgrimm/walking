bde419ca202d55a591d9180ea1c398044586424b
samgrimm <samantha.grimm@gmail.com>
2016-10-23 10:24:19 -0500

    added homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/about'
+
+  get 'static_pages/contact'
+
   devise_for :users , :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :walks
-  root :to => 'walks#index'
+  root :to => 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
