9310d6aea6f0d1ca3063753046d59815203dab27
samgrimm <samantha.grimm@gmail.com>
2016-12-29 15:42:48 -0600

    removed the password sutff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,17 +85,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { :host =>     'desolate-headland-91837.herokuapp.com' }
-  config.action_mailer.delivery_method = :smtp
-config.action_mailer.perform_deliveries = true
-config.action_mailer.raise_delivery_errors = false
-config.action_mailer.default :charset => "utf-8"
-config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
-  domain: "myapp.herokuapp.com",
-  authentication: "plain",
-  enable_starttls_auto: true,
-  user_name: "samantha.grimm@gmail.com",
-  password: "lmfa0677DROGA"
-}
+
 end
