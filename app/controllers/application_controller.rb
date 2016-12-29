1137f2e92a15a911d9508d112193a4b65e787cae
samgrimm <samantha.grimm@gmail.com>
2016-12-29 15:12:58 -0600

    protect from forgery fix from devises website

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json'}
+  protect_from_forgery prepend: true
 
 end
