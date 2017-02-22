d232acdb3eceb362c7143cab0699b3424a359950
Sam <samantha.grimm@gmail.com>
2017-02-22 17:50:41 +0000

    added some notes to the issue model

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,2 +1,5 @@
 class Issue < ApplicationRecord
+    
+    ## DEBUG how are issues getting created? And where are the lat/lng values coming from.
+    ## Check the last issue created on the terminal.
 end
